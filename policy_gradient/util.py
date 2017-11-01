6259ca74f0aa704173e4020af6f5aca5561a0d64
Thanatine <firewings89504@gmail.com>
2017-11-01 15:33:30 +0800

    11/1 backup

--- policy_gradient/util.py
+++ policy_gradient/util.py
@@ -32,6 +32,7 @@ def discount_bootstrap(x, discount_rate, b):
     Sample code should be about 3 lines
     """
     # YOUR CODE >>>>>>>>>>>>>>>>>>>
+    
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<
  
 def plot_curve(data, key, filename=None):
